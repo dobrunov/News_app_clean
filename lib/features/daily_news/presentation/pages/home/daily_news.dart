b3c3d042bb6ec993ac507fd1de88cf3c288f7be4
Evgeniy D <e.dobrunov@gmail.com>
2024-11-30 12:32:53 +0200

    added ui for news page

--- lib/features/daily_news/presentation/pages/home/daily_news.dart
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app_clean/features/daily_news/domain/entities/api_response.dart';
 import 'package:news_app_clean/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
 import 'package:news_app_clean/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
 
@@ -33,12 +35,119 @@ class DailyNews extends StatelessWidget {
         return ListView.builder(
             itemCount: state.response?.articles.length,
             itemBuilder: (context, index) {
-              return ListTile(
-                title: Text('${state.response?.articles[index].title}'),
+              return ArticleWidget(
+                article: state.response?.articles[index],
               );
             });
       }
       return const SizedBox();
     });
+  }
+}
+
+class ArticleWidget extends StatelessWidget {
+  final ArticleModelEntity? article;
+  const ArticleWidget({this.article, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsetsDirectional.only(start: 14, end: 14, top: 14, bottom: 14),
+        height: MediaQuery.of(context).size.width / 2.2,
+        child: Row(
+          children: [
+            _buildImage(context),
+            _buildTitleAndDescription(),
+          ],
+        ));
+  }
+
+  Widget _buildImage(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: article!.urlToImage,
+      imageBuilder: (context, imageProvider) => Padding(
+        padding: const EdgeInsetsDirectional.only(end: 14),
+        child: Container(
+          width: MediaQuery.of(context).size.width / 3,
+          height: double.maxFinite,
+          decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.08),
+            image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+          ),
+        ),
+      ),
+      progressIndicatorBuilder: (context, url, downloadProgress) => Center(
+        child: Padding(
+          padding: const EdgeInsetsDirectional.only(end: 14),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 3,
+              height: double.maxFinite,
+              decoration: BoxDecoration(color: Colors.black.withOpacity(0.08)),
+              child: const CupertinoActivityIndicator(),
+            ),
+          ),
+        ),
+      ),
+      errorWidget: (context, url, error) => Padding(
+        padding: const EdgeInsetsDirectional.only(end: 14),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(20.0),
+          child: Container(
+            width: MediaQuery.of(context).size.width / 3,
+            height: double.maxFinite,
+            decoration: BoxDecoration(color: Colors.black.withOpacity(0.08)),
+            child: const Icon(Icons.error),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTitleAndDescription() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 7.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Title
+            Text(
+              article?.title ?? "",
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontFamily: 'Butler',
+                fontWeight: FontWeight.w900,
+                fontSize: 18,
+                color: Colors.black87,
+              ),
+            ),
+            // Description
+            Expanded(
+                child: Padding(
+              padding: const EdgeInsets.only(top: 4),
+              child: Text(
+                article?.description ?? '',
+                maxLines: 2,
+              ),
+            )),
+            // Datetime
+            Row(
+              children: [
+                const Icon(Icons.timeline_outlined, size: 16),
+                const SizedBox(width: 4),
+                Text(
+                  article!.publishedAt,
+                  style: const TextStyle(fontSize: 12),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
