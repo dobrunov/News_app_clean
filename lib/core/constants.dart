17ac9353d415a52ac515d83494d71b96a5a914fd
Evgeniy D <e.dobrunov@gmail.com>
2024-11-30 13:02:48 +0200

    added class with key

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,6 @@
+import 'package:news_app_clean/core/secret_class.dart';
+
 const String newsAPIBaseURL = 'https://newsapi.org/v2';
-const String newsAPIKey = 'e50d9fefe9304943b9c765a5a009af71';
+String newsAPIKey = SecretClass.newsAPIkey;
 const String countryQuery = 'us';
 const String categoryQuery = 'general';
